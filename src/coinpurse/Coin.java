94e30843122661f3edc8a6dc44730c49bf8a8bd2
pawan1235 <pawan1235@hotmail.co.th>
2018-01-19 22:20:22 +0700

    Add java doc for coin class

--- src/coinpurse/Coin.java
+++ src/coinpurse/Coin.java
@@ -1,26 +1,54 @@
 package coinpurse;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
-
+/**
+ * A class to create coin with its value and currency.
+ * 
+ * @author Pawan Intawongsa
+ *
+ */
 public class Coin implements Comparable<Coin> {
+	/** value of coin */
 	private double value;
+	/** currency of coin */
 	private String currency;
 
+	/**
+	 * Create a coin with value and currency.
+	 * 
+	 * @param value
+	 *            is value of coin
+	 * @param currency
+	 *            is currency of coin
+	 */
 	public Coin(double value, String currency) {
 		this.value = value;
 		this.currency = currency;
 	}
 
+	/**
+	 * Get value of coin
+	 * 
+	 * @return value of coin
+	 */
 	public double getValue() {
 		return value;
 	}
 
+	/**
+	 * Get currency of coin
+	 * 
+	 * @return currency of coin
+	 */
 	public String getCurrency() {
 		return currency;
 	}
 
+	/**
+	 * Check 2 coins if its equal each other or not.
+	 * 
+	 * @param arg
+	 *            is an object to use to check if it equal coin.
+	 */
 	@Override
 	public boolean equals(Object arg) {
 		if (arg == null)
@@ -34,6 +62,12 @@ public class Coin implements Comparable<Coin> {
 			return false;
 	}
 
+	/**
+	 * Compare 2 coin if they are more or less or equal to each other.
+	 * 
+	 * @param coin
+	 *            is another coin to use to compare
+	 */
 	public int compareTo(Coin coin) {
 		if (this.getValue() < coin.getValue())
 			return 1;
@@ -42,32 +76,13 @@ public class Coin implements Comparable<Coin> {
 		return 0;
 	}
 
+	/**
+	 * Return coin's description
+	 * 
+	 * @return coin's description
+	 */
 	@Override
 	public String toString() {
 		return this.value + "-" + this.currency;
-	}
-
-	public static void main(String[] args) {
-		List<Coin> coins = new ArrayList<Coin>();
-		List<Coin> temp = new ArrayList<Coin>();
-		coins.add(new Coin(10.0, "Baht"));
-		coins.add(new Coin(0.5, "Baht"));
-		coins.add(new Coin(2.0, "Baht")); // the most hated coin
-		coins.add(new Coin(1.0, "Baht"));
-		coins.add(new Coin(50, "Rupe"));
-		coins.add(new Coin(50, "Baht"));
-		printCoins(coins);
-		// This static method sorts any list of Comparable objects
-		System.out.println("---------------------------------");
-		Collections.sort(coins);
-		temp = coins;
-		printCoins(temp); // the coins should be sorted by value now
-
-	}
-
-	private static void printCoins(List<Coin> coins) {
-		for (Coin coin : coins) {
-			System.out.println(coin.getValue() + "-" + coin.getCurrency());
-		}
 	}
 }
