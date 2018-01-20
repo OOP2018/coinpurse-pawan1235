df48c042432c57a1594aa6bb4317dc536d3e4e77
pawan1235 <pawan1235@hotmail.co.th>
2018-01-20 20:25:41 +0700

    fix filterByCurrency

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -4,30 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Class that contain a useful method for List of Coin it can return List of coin that all same currency,
- * Sort list of Coin
+ * Class that contain a useful method for List of Coin it can return List of
+ * coin that all same currency, Sort list of Coin
  * 
  * @author Pawan Intawongsa
  *
  */
 public class MoneyUtil {
 	/**
-	 * Return a List of Coins that contains only the coins
-	 *from coins (the parameter) that have same currency
- 	 *as the currency parameter.
+	 * Return a List of Coins that contains only the coins from coins (the
+	 * parameter) that have same currency as the currency parameter.
 	 * 
 	 * @param coins
-	 * 				is List of Coin to be filtered
+	 *            is List of Coin to be filtered
 	 * @param currency
-	 * 				is Currency that we want to be return
-	 * @return A List of Coins that contains only the coins
-	 *from coins (the parameter) that have same currency
- 	 *as the currency parameter.
+	 *            is Currency that we want to be return
+	 * @return A List of Coins that contains only the coins from coins (the
+	 *         parameter) that have same currency as the currency parameter.
 	 */
 	public static List<Coin> filterByCurrency(List<Coin> coins, String currency) {
 		List<Coin> temp = new ArrayList<Coin>();
 		for (Coin coin : coins) {
-			if (coin.getCurrency().equals(currency)) {
+			if (coin.getCurrency() == currency) {
 				temp.add(coin);
 			}
 		}
@@ -35,10 +33,11 @@ public class MoneyUtil {
 	}
 
 	/**
-	 * Sort a list of coins and print the result on the
-	 * console. Write a separate method to print the list
+	 * Sort a list of coins and print the result on the console. Write a separate
+	 * method to print the list
+	 * 
 	 * @param coins
-	 * 				is List of coin to be sort
+	 *            is List of coin to be sort
 	 */
 	public static void sortCoins(List<Coin> coins) {
 		java.util.Collections.sort(coins);
